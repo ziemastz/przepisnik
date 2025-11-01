4bfc234461f0d6fb3e8380b8fdd5b37f6470d121
Tomasz Z <40549003+ziemastz@users.noreply.github.com>
2025-11-01 19:53:56 +0100

    Add clear project (#1)

---
+++ src/main/java/JWD/Przepisnik/PrzepisnikApplication.java
@@ -0,0 +1,13 @@
+package JWD.Przepisnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrzepisnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrzepisnikApplication.class, args);
+	}
+
+}
