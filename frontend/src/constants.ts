24e4a996bf7931ab1e6b8239dad5b0fe39232dbd
Tomasz Z <40549003+ziemastz@users.noreply.github.com>
2026-03-23 11:27:25 +0100

    Dodanie przyciskow

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -3,6 +3,10 @@ const constants = {
     footer: {
         rightsText: '\u00A9 2025 Przepi\u015Bnik. All rights reserved.',
     },
+    dashboardUser:
+    {
+        welcomeText: 'Witaj szefie!',
+    }
 };
 
 export default constants;
